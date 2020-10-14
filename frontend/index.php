ff211bcf10888f786d294f0d5a147e2af49a89a5
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2020-10-14 16:49:17 +0700

    Thêm phần carouse

--- frontend/index.php
+++ frontend/index.php
@@ -23,7 +23,65 @@ include_once(__DIR__ . '/../dbconnect.php');
     </div>
     <?php include_once(__DIR__ . '/layouts/partials/header.php'); ?>
     <!-- Phần nội dung trang web -->
-    
+    <!-- Phần Carouse -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 p-0 m-0">
+                <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+                    <?php
+                    $sqlKhuyenMai = "SELECT * FROM khuyenmai WHERE km_denngay >= NOW();";
+                    $resultKhuyenMai = mysqli_query($conn, $sqlKhuyenMai);
+                    $dataKhuyenMai = [];
+                    $i = 1;
+                    while ($row = mysqli_fetch_array($resultKhuyenMai, MYSQLI_ASSOC)) {
+                        $dataKhuyenMai[] = array(
+                            'km_index' => $i++,
+                            'km_id' => $row['km_id'],
+                            'km_ten' => $row['km_ten'],
+                            'km_noidung' => $row['km_noidung'],
+                            'km_tungay' => $row['km_tungay'],
+                            'km_denngay' => $row['km_denngay'],
+                            'km_anh' => $row['km_anh'],
+                        );
+                    }
+                    ?>
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                        <?php foreach ($dataKhuyenMai as $km) : ?>
+                            <li data-target="#carouselExampleCaptions" data-slide-to="<?= $km['km_index'] ?>"></li>
+                        <?php endforeach; ?>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="/templatedoan/anh-do-an/km3.jpg" class="d-block w-100" alt="">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h1 class="text-shadow">Chào mừng đến với SHOPHOA</h1>
+                            </div>
+                        </div>
+                        <?php foreach ($dataKhuyenMai as $km) : ?>
+                            <div class="carousel-item">
+                                <img src="/templatedoan/anh-do-an/<?= $km['km_anh'] ?>" class="d-block w-100" alt="<?= $km['km_ten'] ?>">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <a class="text-white" href="sukien.php?km_id=<?= $km['km_id'] ?>">
+                                        <h1 class="text-shadow"><?= $km['km_ten'] ?></h1>
+                                    </a>
+                                </div>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End phần Carouse -->
     <!-- End phần nội dung trang web -->
     <?php include_once(__DIR__ . '/layouts/partials/footer.php'); ?>
 
