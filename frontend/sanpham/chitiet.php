59e768b78a004532335e894d221c51baa872342b
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2020-10-16 19:33:32 +0700

    update

--- frontend/sanpham/chitiet.php
+++ frontend/sanpham/chitiet.php
@@ -197,86 +197,11 @@ else
         </div>
         <div class="row mt-5">
             <div class="col-md-12">
+            <?php
+            
+            ?>
                 <h3 class="text-danger myfont">Sản phẩm liên quan</h3>
                 <div class="row row-cols-md-4 row-cols-2">
-                    <div class="col py-3">
-                        <div class="card my-card">
-                            <a href="chitiet.php">
-                                <div class="my-box-card-img">
-                                    <img src="/templatedoan/imgs/BoHoaCrystalPearlonweb_1024x1024@2x.jpg" alt="" class="card-img-top my-card-img img-show">
-                                    <img src="/templatedoan/imgs/BoHoaCrystalPearl_d764aec2-22bb-4758-a90f-a5ade95d798c_1024x1024@2x.jpg" alt="" class="card-img-top my-card-img img-hide">
-                                    <div class="text-danger danh_gia">
-                                        <i class="fa fa-star" aria-hidden="true"></i>
-                                        <i class="fa fa-star" aria-hidden="true"></i>
-                                        <i class="fa fa-star" aria-hidden="true"></i>
-                                        <i class="fa fa-star-half-o" aria-hidden="true"></i>
-                                        <i class="fa fa-star-o" aria-hidden="true"></i>
-                                    </div>
-                                </div>
-                            </a>
-                            <div class="card-body px-0">
-                                <a href="chitiet.php" class="card-title my-card-title font-weight-bold">
-                                    Bó hoa Crystal Pearl
-                                </a>
-                                <h5 class="my-3">
-                                    <span class="text-secondary"><s>150,000 VNĐ</s></span> <span class="text-danger">130,000 VNĐ</span>
-                                </h5>
-                                <button class="btn myfont text-danger btn-add">Thêm vào giỏ hàng</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col py-3">
-                        <div class="card my-card">
-                            <a href="chitiet.php">
-                                <div class="my-box-card-img">
-                                    <img src="/templatedoan/imgs/BoHoaCrystalPearlonweb_1024x1024@2x.jpg" alt="" class="card-img-top my-card-img img-show">
-                                    <img src="/templatedoan/imgs/BoHoaCrystalPearl_d764aec2-22bb-4758-a90f-a5ade95d798c_1024x1024@2x.jpg" alt="" class="card-img-top my-card-img img-hide">
-                                    <div class="text-danger danh_gia">
-                                        <i class="fa fa-star" aria-hidden="true"></i>
-                                        <i class="fa fa-star" aria-hidden="true"></i>
-                                        <i class="fa fa-star" aria-hidden="true"></i>
-                                        <i class="fa fa-star-half-o" aria-hidden="true"></i>
-                                        <i class="fa fa-star-o" aria-hidden="true"></i>
-                                    </div>
-                                </div>
-                            </a>
-                            <div class="card-body px-0">
-                                <a href="chitiet.php" class="card-title my-card-title font-weight-bold">
-                                    Bó hoa Crystal Pearl
-                                </a>
-                                <h5 class="my-3">
-                                    <span class="text-secondary"><s>150,000 VNĐ</s></span> <span class="text-danger">130,000 VNĐ</span>
-                                </h5>
-                                <button class="btn myfont text-danger btn-add">Thêm vào giỏ hàng</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col py-3">
-                        <div class="card my-card">
-                            <a href="chitiet.php">
-                                <div class="my-box-card-img">
-                                    <img src="/templatedoan/imgs/BoHoaCrystalPearlonweb_1024x1024@2x.jpg" alt="" class="card-img-top my-card-img img-show">
-                                    <img src="/templatedoan/imgs/BoHoaCrystalPearl_d764aec2-22bb-4758-a90f-a5ade95d798c_1024x1024@2x.jpg" alt="" class="card-img-top my-card-img img-hide">
-                                    <div class="text-danger danh_gia">
-                                        <i class="fa fa-star" aria-hidden="true"></i>
-                                        <i class="fa fa-star" aria-hidden="true"></i>
-                                        <i class="fa fa-star" aria-hidden="true"></i>
-                                        <i class="fa fa-star-half-o" aria-hidden="true"></i>
-                                        <i class="fa fa-star-o" aria-hidden="true"></i>
-                                    </div>
-                                </div>
-                            </a>
-                            <div class="card-body px-0">
-                                <a href="chitiet.php" class="card-title my-card-title font-weight-bold">
-                                    Bó hoa Crystal Pearl
-                                </a>
-                                <h5 class="my-3">
-                                    <span class="text-secondary"><s>150,000 VNĐ</s></span> <span class="text-danger">130,000 VNĐ</span>
-                                </h5>
-                                <button class="btn myfont text-danger btn-add">Thêm vào giỏ hàng</button>
-                            </div>
-                        </div>
-                    </div>
                     <div class="col py-3">
                         <div class="card my-card">
                             <a href="chitiet.php">
