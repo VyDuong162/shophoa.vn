f7bb4f29104a26d81b772ad18e5c94fb7bb3e864
leokaa <linhduy8a5@gmail.com>
2020-10-19 22:37:23 +0700

    update-sanpham--->2

--- backend/functions/sanpham_2/delete.php
+++ backend/functions/sanpham_2/delete.php
@@ -1,6 +1,6 @@
 <?php
-// Truy vấn database
-// 1. Include file cấu hình kết nối đến database, khởi tạo kết nối $conn
+// Truy v?n database
+// 1. Include file c?u h?nh k?t n?i đ?n database, kh?i t?o k?t n?i $conn
 include_once(__DIR__ . '/../../../dbconnect.php');
 $sp_id = $_GET['sp_id'];
 $sqlSanpham = "DELETE FROM `sanpham` WHERE sp_id=" . $sp_id;
@@ -8,5 +8,3 @@ $result = mysqli_query($conn, $sql);
 mysqli_close($conn);
 header('location:index.php');
 ?>
-
-
