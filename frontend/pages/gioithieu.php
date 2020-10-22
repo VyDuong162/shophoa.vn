5fbedc8731b15102d2ca66fd346f2ab340be0847
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2020-10-22 16:10:31 +0700

    thêm nội dung trang giới thiệu

--- frontend/pages/gioithieu.php
+++ frontend/pages/gioithieu.php
@@ -23,6 +23,17 @@ include_once(__DIR__ . '/../../dbconnect.php');
     </div>
     <?php include_once(__DIR__ . '/../layouts/partials/header.php'); ?>
     <!-- Phần nội dung trang web -->
+    <div class="container my-3">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h3 class="myfont text-danger">Về chúng tôi</h3>
+                <p>Sứ mệnh của chúng tôi là giúp bạn trao đi tâm tư và biến mọi dịp trọng đại của bạn trở nên đặc biệt hơn.</p>
+                <p>Đội ngũ giàu kinh nghiệm của chúng tôi luôn đảm bảo đem đến cho bạn những bó hoa tươi nhất được gói cùng các loại nguyên vật liệu chất lượng cao. Chúng tôi cung cấp nhiều chủng loại hoa, kích thước và thiết kế khác nhau phù hợp cho bất kỳ dịp nào bạn cần.</p>
+                <p>Thông qua nền tảng trực tuyến, chúng tôi mong muốn đem đến cho bạn trải nghiệm thật tiện lợi, dễ dàng và tràn đầy niềm vui.</p>
+                <img src="/shophoa.vn/assets/shared/img/aboutus.jpg" alt="Về chúng tôi" width="80%">
+            </div>
+        </div>
+    </div>
     <!-- End phần nội dung trang web -->
     <?php include_once(__DIR__ . '/../layouts/partials/footer.php'); ?>
 
