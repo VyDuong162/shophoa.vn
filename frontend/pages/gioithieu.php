7d53d6444bdfd878d8f62ede2bb277f4ab97b7e4
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2020-10-22 16:01:06 +0700

    Create gioithieu.php

---
+++ frontend/pages/gioithieu.php
@@ -0,0 +1,32 @@
+<?php
+if (session_id() === '') {
+    session_start();
+}
+include_once(__DIR__ . '/../../dbconnect.php');
+?>
+<!DOCTYPE html>
+<html lang="vn">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shop Hoa | Giới thiệu</title>
+    <?php include_once(__DIR__ . '/../layouts/styles.php'); ?>
+</head>
+
+<body>
+    <!-- Phần loading trang web -->
+    <div id="load">
+        <div class="spinner-border" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </div>
+    <?php include_once(__DIR__ . '/../layouts/partials/header.php'); ?>
+    <!-- Phần nội dung trang web -->
+    <!-- End phần nội dung trang web -->
+    <?php include_once(__DIR__ . '/../layouts/partials/footer.php'); ?>
+
+    <?php include_once(__DIR__ . '/../layouts/scripts.php'); ?>
+</body>
+
+</html>
