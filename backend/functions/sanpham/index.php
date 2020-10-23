7f7306467960fb585cf9bb4aaa1eca674c3c82b4
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2020-10-23 22:14:20 +0700

    Update index.php

--- backend/functions/sanpham/index.php
+++ backend/functions/sanpham/index.php
@@ -17,6 +17,12 @@ include_once(__DIR__ . '/../../../dbconnect.php');
     <link rel="stylesheet" href="/shophoa.vn/assets/vendor/DataTables/datatables.min.css" type="text/css">
     <link rel="stylesheet" href="/shophoa.vn/assets/vendor/DataTables/DataTables/css/dataTables.bootstrap4.min.css" type="text/css">
     <link rel="stylesheet" href="/shophoa.vn/assets/vendor/DataTables/Buttons/css/buttons.bootstrap4.min.css" type="text/css">
+    <link rel="stylesheet" href="/shophoa.vn/assets/vendor/fancybox/jquery.fancybox.min.css">
+    <style>
+        ul{
+            list-style-type: none;
+        }
+    </style>
 </head>
 
 <body>
@@ -28,12 +34,45 @@ include_once(__DIR__ . '/../../../dbconnect.php');
             </div>
             <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
                 <?php
-                    $sqlSanPham = "SELECT * FROM sanpham;";
-                    $resultSanPham = mysqli_query($conn, $sqlSanPham);
-                    $dataSanPham = [];
-                    
+                $sqlSanPham = "SELECT * FROM sanpham AS a LEFT JOIN khuyenmai AS b ON a.km = b.km_id;";
+                $resultSanPham = mysqli_query($conn, $sqlSanPham);
+                $dataSanPham = [];
+                while ($rowSanPham = mysqli_fetch_array($resultSanPham, MYSQLI_ASSOC)) {
+                    $km_tomtat = '';
+                    if (!empty($rowSanPham['km_ten'])) {
+                        $km_tomtat = sprintf("%s (%s-%s)", $rowSanPham['km_ten'], date('d/m/Y', strtotime($rowSanPham['km_tungay'])), date('d/m/Y', strtotime($rowSanPham['km_denngay'])));
+                    }
+                    $sqlLoaiHoa = "SELECT a.lh_ten FROM loaihoa AS a, sanpham_has_loaihoa AS b WHERE a.lh_id = b.loaihoa_lh_id AND b.sanpham_sp_id = {$rowSanPham['sp_id']};";
+                    $resultLoaiHoa = mysqli_query($conn, $sqlLoaiHoa);
+                    $loaiHoa = '<ul> ';
+                    while($rowLoaiHoa = mysqli_fetch_array($resultLoaiHoa, MYSQLI_ASSOC)){
+                        $loaiHoa .= '<li>'.$rowLoaiHoa['lh_ten'].'</li>';
+                    }
+                    $loaiHoa .= '</ul>';
+                    $sqlMauHoa = "SELECT a.mh_ten FROM mauhoa AS a, sanpham_has_mauhoa AS b WHERE a.mh_id = b.mauhoa_mh_id AND b.sanpham_sp_id = {$rowSanPham['sp_id']};";
+                    $resultMauHoa = mysqli_query($conn, $sqlMauHoa);
+                    $mauHoa = '<ul> ';
+                    while($rowMauHoa = mysqli_fetch_array($resultMauHoa, MYSQLI_ASSOC)){
+                        $mauHoa .= '<li>'.$rowMauHoa['mh_ten'].'</li>';
+                    }
+                    $mauHoa .= '</ul>';
+                    $dataSanPham[] = array(
+                        'sp_id' => $rowSanPham['sp_id'],
+                        'sp_ten' => $rowSanPham['sp_ten'],
+                        'sp_gia' => number_format($rowSanPham['sp_gia'], 0, ".", ","),
+                        'sp_giacu' => ($rowSanPham['sp_giacu'] > 0) ? number_format($rowSanPham['sp_giacu'], 0, ".", ",") : '',
+                        'sp_yeuthich' => $rowSanPham['sp_yeuthich'],
+                        'sp_mota_ngan' => $rowSanPham['sp_mota_ngan'],
+                        'sp_mota_chitiet' => $rowSanPham['sp_mota_chitiet'],
+                        'sp_ngaycapnhat' => date('d/m/Y', strtotime($rowSanPham['sp_ngaycapnhat'])),
+                        'sp_avt_tenfile' => $rowSanPham['sp_avt_tenfile'],
+                        'km_tomtat' => $km_tomtat,
+                        'loaiHoa' => $loaiHoa,
+                        'mauHoa' => $mauHoa,
+                    );
+                }
                 ?>
-                <div class="row ">
+                <div class="row">
                     <div class="col-md-12 text-right mt-3">
                         <a href="create.php"><button type="button" class="btn btn-primary">Thêm mới</button></a> <br><br>
                     </div>
@@ -47,28 +86,44 @@ include_once(__DIR__ . '/../../../dbconnect.php');
                             <table id="tblDanhSach" class="table mx-auto table-bordered ">
                                 <thead class="thead-dark">
                                     <tr class="text-center">
-                                        <th class="align-middle text-center">Mã hoa</th>
-                                        <th class="align-middle text-center">Tên hoa</th>
-                                        <th class="align-middle text-center">Loại hoa</th>
-                                        <th class="align-middle text-center">Chủ đề</th>
-                                        <th class="align-middle text-center">Màu hoa</th>
-                                        <th class="align-middle text-center">Giá hoa</th>
-                                        <th class="align-middle text-center">Khuyến mãi</th>
-                                        <th class="align-middle text-center">Ngày cập nhật</th>
-                                        <th class="align-middle text-center">Thực thi</th>
+                                        <th>STT</th>
+                                        <th>Tên</th>
+                                        <th>Giá</th>
+                                        <th>Giá cũ</th>
+                                        <th>Yêu thích</th>
+                                        <th>Loại hoa</th>
+                                        <th>Màu hoa</th>
+                                        <th>Chủ đề hoa</th>
+                                        <th>Ngày cập nhật</th>
+                                        <th>Khuyến mãi</th>
+                                        <th>Ảnh</th>
+                                        <th>Thực thi</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <?php foreach ($data as $sanpham) : ?>
+                                    <?php $i = 1; ?>
+                                    <?php foreach ($dataSanPham as $sp) : ?>
                                         <tr>
-                                            <td class="align-middle text-center"><?= $sanpham['sp_id'] ?></td>
-                                            <td class="align-middle text-center"><?= $sanpham['sp_ten'] ?></td>
-                                            <td class="align-middle text-center"><?= $sanpham['lh_ten'] ?></td>
-                                            <td class="align-middle text-center"><?= $sanpham['cd_ten'] ?></td>
-                                            <td class="align-middle text-center"><?= $sanpham['mh_ten'] ?></td>
-                                            <td class="align-middle text-center"><?= $sanpham['sp_gia'] ?></td>
-                                            <td class="align-middle text-center"><?= $sanpham['km_tomtat'] ?></td>
-                                            <td class="align-middle text-center"><?= $sanpham['sp_ngaycapnhat'] ?></td>
+                                            <td class="text-center align-middle"><?= $i++; ?></td>
+                                            <td class="text-center align-middle"><?= $sp['sp_ten']; ?></td>
+                                            <td class="text-center align-middle"><?= $sp['sp_gia']; ?></td>
+                                            <td class="text-center align-middle"><?= $sp['sp_giacu']; ?></td>
+                                            <td class="text-center align-middle"><?= $sp['sp_yeuthich']; ?></td>
+                                            <td class="text-center align-middle"><?= $sp['loaiHoa']; ?></td>
+                                            <td class="text-center align-middle"><?= $sp['mauHoa']; ?></td>
+                                            <td class="text-center align-middle"><?= $sp['sp_ngaycapnhat']; ?></td>
+                                            <td class="text-center align-middle"><?= $sp['km_tomtat']; ?></td>
+                                            <td class="text-center align-middle">
+                                                <?php if (!file_exists("../../../assets/uploads/img-product/" . $sp['sp_avt_tenfile']) || empty($sp['sp_avt_tenfile'])) : ?>
+                                                    <a data-fancybox="gallery" href="/shophoa.vn/assets/shared/img/default.png" data-caption="[Ảnh mặc định] <?= $sp['sp_ten'] ?>">
+                                                        <img src="/shophoa.vn/assets/shared/img/default.png" class="img-fluid" width="100px">
+                                                    </a>
+                                                <?php else : ?>
+                                                    <a data-fancybox="gallery" href="/shophoa.vn/assets/uploads/img-product/<?= $sp['sp_avt_tenfile'] ?>" data-caption="[<?= $sp['hasp_id'] ?>] <?= $sp['sp_ten'] ?>">
+                                                        <img src="/shophoa.vn/assets/uploads/img-product/<?= $sp['sp_avt_tenfile'] ?>" class="img-fluid" width="100px" />
+                                                    </a>
+                                                <?php endif ?>
+                                            </td>
                                             <td class="align-middle text-center">
                                                 <a href="edit.php?sp_id=<?= $sanpham['sp_id']; ?>" class="btn btn-danger" data-toggle="tooltip" data-placement="top" title="Sửa">
                                                     <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
@@ -94,6 +149,7 @@ include_once(__DIR__ . '/../../../dbconnect.php');
     <script src="/shophoa.vn/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="/shophoa.vn/assets/vendor/sweetalert/sweetalert.min.js"></script>
     <script src="/shophoa.vn/assets/vendor/DataTables/DataTables/js/dataTables.bootstrap4.min.js"></script>
+    <script src="/shophoa.vn/assets/vendor/fancybox/jquery.fancybox.min.js"></script>
     <script>
         $(document).ready(function() {
             $('#tblDanhSach').DataTable({
