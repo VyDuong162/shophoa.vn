a10da6a12f0dbba3da9c4851befb481699a15457
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2020-10-26 21:40:34 +0700

    Update create.php

--- backend/functions/hinhthucthanhtoan/create.php
+++ backend/functions/hinhthucthanhtoan/create.php
@@ -59,7 +59,7 @@ if (session_id() === '') {
                                 </div>
                             </div>
                             <div class="col-md-12 text-center mb-5">
-                                <button class="btn btn-success" name="btnsave" id="btnsave" type="submit">Lưu dữ liệu</button>
+                                <button class="btn btn-success" name="btnsave" id="btnsave" type="submit">Thêm</button>
                             </div>
                         </div>
                     </form>
@@ -68,27 +68,46 @@ if (session_id() === '') {
                 if (isset($_POST['btnsave'])) {
                     $httt_ten = $_POST['httt_ten'];
                     $erorrs = [];
-                    if (empty( $httt_ten)) {
+                    if (empty($httt_ten)) {
                         $erorrs['httt_ten'][] = [
                             'rule' => 'required',
                             'rule_value' => true,
                             'value' =>  $httt_ten,
-                            'mes' => 'không được bỏ trống',
+                            'mes' => 'Tên hình thức thanh toán không được bỏ trống',
                         ];
                     } else {
                         if (strlen($httt_ten) > 50) {
                             $erorrs['httt_ten'][] = [
                                 'rule' => 'maxlength',
+                                'rule_value' => 50,
+                                'value' => $httt_ten,
+                                'mes' => 'Tên hình thức thanh toán chỉ được tối đa 50 ký tự',
+                            ];
+                        }
+                        if (strlen($httt_ten) < 3) {
+                            $erorrs['httt_ten'][] = [
+                                'rule' => 'minlength',
                                 'rule_value' => 3,
                                 'value' => $httt_ten,
-                                'mes' => 'Tên chỉ được tối đa 50 ký tự',
+                                'mes' => 'Tên hình thức thanh toán phải tối thiểu 3 ký tự',
                             ];
                         }
                     }
                 }
                 ?>
+                <?php if (isset($_POST['btnsave']) && (isset($erorrs) && !empty($erorrs))) : ?>
+                    <div class="alert alert-warning col-md-12" role="alert">
+                        <ul>
+                            <?php foreach ($erorrs as $loi) : ?>
+                                <?php foreach ($loi as $a) : ?>
+                                    <li><?= $a['mes'] ?></li>
+                                <?php endforeach; ?>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
+                <?php endif; ?>
                 <?php
-                if (isset($_POST['btnsave'])) {
+                if (isset($_POST['btnsave']) && !(isset($erorrs) && !empty($erorrs))) {
                     $httt_ten = htmlentities($_POST['httt_ten']);
                     // Câu lệnh INSERT
                     $sql = "INSERT INTO `hinhthucthanhtoan` (httt_ten) VALUES ('$httt_ten');";
@@ -106,6 +125,42 @@ if (session_id() === '') {
     </div>
     <?php include_once(__DIR__ . '/../../layouts/partials/footer.php'); ?>
     <?php include_once(__DIR__ . '/../../layouts/scripts.php'); ?>
+    <script>
+        $(document).ready(function() {
+            $('#frmthemmoi').validate({
+                rules: {
+                    httt_ten: {
+                        required: true,
+                        minlength: 3,
+                        maxlength: 50,
+                    },
+                },
+                messages: {
+                    httt_ten: {
+                        required: "Bạn phải nhập tên hình thức thanh toán",
+                        minlength: "Bạn phải nhập họ tên tối thiểu 3 ký tự",
+                        maxlength: "Bạn chỉ được nhập họ tên tối đa 50 ký tự",
+                    },
+                },
+                errorElement: "em",
+                errorPlacement: function(error, element) {
+                    error.addClass("invalid-feedback");
+                    if (element.prop("type") === "checkbox") {
+                        error.insertAfter(element.parent("label"));
+                    } else {
+                        error.insertAfter(element);
+                    }
+                },
+                success: function(label, element) {},
+                highlight: function(element, errorClass, validClass) {
+                    $(element).addClass("is-invalid").removeClass("is-valid");
+                },
+                unhighlight: function(element, errorClass, validClass) {
+                    $(element).addClass("is-valid").removeClass("is-invalid");
+                }
+            });
+        });
+    </script>
 </body>
 
 </html>
